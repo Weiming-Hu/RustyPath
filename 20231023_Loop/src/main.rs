0807b969e561e5de429dfdf9968d7b00cccbc599
Weiming Hu <huweiming950714@gmail.com>
2023-11-23 00:29:42 -0500

    new projects

---
+++ 20231023_Loop/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let mut count = 0;
+
+    'couting_up: loop {
+        println!("count = {count}");
+
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'couting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
