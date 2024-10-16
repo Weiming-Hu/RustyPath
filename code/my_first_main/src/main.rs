03095f4ac6155b507a8549fcca7b5e106acb5ea8
Weiming-Hu <weiming@psu.edu>
2024-10-16 16:31:04 -0400

    completing assignments on Udemy

--- code/my_first_main/src/main.rs
+++ code/my_first_main/src/main.rs
@@ -1,5 +1,20 @@
 fn main() {
-    println!("Hello, world!");
-    println!("Happy I am");
-    let x = 1 + 1;
+    let x = 3;
+    let y = 4;
+    println!(
+        "The result of x+3 times y+5 is {}",
+        times(add_3(x), add_5(y))
+    );
+}
+
+fn add_3(x: i16) -> i16 {
+    x + 3
+}
+
+fn add_5(x: i16) -> i16 {
+    x + 5
+}
+
+fn times(x: i16, y: i16) -> i16 {
+    x * y
 }
